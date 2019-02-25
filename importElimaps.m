53f2573165ea2c36c5f8bfad2b918e009dc5d7cd
irasama <41342852+irasama@users.noreply.github.com>
2019-02-25 09:55:11 -0800

    function importElimaps
    Importar los mapas desde los diferentes paths

---
+++ importElimaps.m
@@ -0,0 +1,11 @@
+function [data]=importElimaps(path)
+files = dir([path '/*.tif']);          
+dataname = {files.name}';
+map = imread([files(1).folder '/' files(1).name]);
+[m,n]=size(map);
+data = ones(m,n,length(files));
+for i=1:length(files)
+    map = imread([files(i).folder '/' files(i).name]);
+    io=map==-9999; map(io)=NaN;
+    data(:,:,i)=map;
+end
